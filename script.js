153b62244c80037383dd318c095dc331c24f0fa1
yasedrisian <yasedrisian-KnNIvq>
2023-01-30 19:14:45 -0500

    thirddcommit

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const quizecontainer = document.getelementsbyId('quize')
-const scorecontainer =document.getElementById('answer')
-const submitbuttom = document.getElementById('submit')
-const questioncontainer = document.getElementById('Questions')
-
-
-
 function start() {
     document.getElementById('content').style = "";
     document.getElementById('start_btn').style = 'display: none;';
@@ -26,46 +19,30 @@ function Quiz() {
     var q1 = document.forms['qForm']['q1'].value;
     var q2 = document.forms['qForm']['q2'].value;
     var q3 = document.forms['qForm']['q3'].value;
-    //Timer Variables and Timer
-var time_remaining = 90;
-var time= true;
-var timerStart = false;
-var countDownTimer = document.getElementById("countDownTimer");
 
-//Count down by seconds
-var countDown = setInterval(setCountDown, 1000);
-
-function setCountDown(){
-    if(timerStart)
-    time_remaining--;
-    if(time_remaining<=0){
-        quiz_Over();
-    time_remaining = 0;
-    }
-    document.getElementById("timer").innerHTML = time_remaining;
-};
-
-    // for (var i = 1; i <= total; i++) {
-    //     if (eval('q' + i) == null || eval('q' + i) == '') {
-    //         alert("Can not be empty" + i);
-    //         return false;
-    //     }
-    // }
+    
     var username = document.getElementById("UserName").value;
     
-    var answers = ['a', 'd', 'b'];
+    var answers = ['a', 'd' ,'b'];
 
     for (var i = 1; i <= total; i++) {
         if (eval('q' + i) == answers[i - 1]) {
             score++;
         }
+
+        if (i>total){
+        clearTimeout(out);
+        }
+
     }
+    
 
     var Users = new Array(username);
     for (var i = 0; i <= Users.length; i++) {
         var result = document.getElementById('result');
         result.innerHTML = username + " Your score is " + score + " out of " + total;
     }
+    
 }
 
 
@@ -73,23 +50,24 @@ function setCountDown(){
 function timer() {
     document.getElementById('timer').innerHTML =
 
-        01 + ":" + 15;
+        01 + ":" + 01;
 
     startTimer();
+    
 
 
 
-    //function startTimer() {
+    function startTimer() {
 
-      //  var presentTime = document.getElementById('timer').innerHTML;
+        var presentTime = document.getElementById('timer').innerHTML;
 
-       // var timeArray = presentTime.split(/[:]+/);
+        var timeArray = presentTime.split(/[:]+/);
 
-      //  var m = timeArray[0];
+        var m = timeArray[0];
 
-       // var s = checkSecond((timeArray[1] - 1));
+        var s = checkSecond((timeArray[1] - 1));
 
-       // if (s == 59) { m = m - 1 }
+        if (s == 59) { m = m - 1 }
 
 
 
@@ -105,7 +83,7 @@ function timer() {
 
         if (m < 0) {
             clearTimeout(out);
-            alert('Time is done. Plese Enter Your Name');
+            alert('Timer is done. Plese Enter Your Name');
 
             var elements = document.getElementsByName('q1');
             var elements2 = document.getElementsByName('q2');
@@ -113,12 +91,15 @@ function timer() {
                 elements[i].style = "display :none";
                 elements2[i].style = "display :none";
             }
+            
+
 
 
         }
 
     }
-
+    
+    
 
 
     function checkSecond(sec) {
@@ -129,5 +110,7 @@ function timer() {
 
         return sec;
 
-    }
 
+    }
+    stopfunction
+}
